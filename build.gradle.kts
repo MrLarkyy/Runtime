9aa85da340fe14ea7cdecf790dd4db4541e2f35f
MrLarkyy_ <studiotadikl@gmail.com>
2026-02-01 13:00:32 +0100

    Downgrade Kotlin plugin version to 2.2.21 and bump project version to 26.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "2.3.0" apply false
+    kotlin("jvm") version "2.2.21" apply false
     id("com.gradle.plugin-publish") version "1.3.1" apply false
 }
 
 allprojects {
     group = "gg.aquatic"
-    version = "26.0.1"
+    version = "26.0.2"
 
     repositories {
         mavenCentral()
